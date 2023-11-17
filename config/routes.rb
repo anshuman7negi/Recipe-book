454f4b8ffb2ceae91f4c19de1e28d0119e57eeca
anshuman7negi <anshumannegi777@gmail.com>
2023-11-17 20:01:10 +0530

    added  devise to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'foods#index', as: :authenticated_root
+    end
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :foods, only: [:index]
+  root to: 'foods#index'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end

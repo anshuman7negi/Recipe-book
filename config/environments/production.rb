3faa3800815df7d48b74da5a289e3552ac03dade
anshuman7negi <anshumannegi777@gmail.com>
2023-11-19 12:42:49 +0530

    update render.yml file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,17 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'recipe-book-x8m1.onrender.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'anshumannegi777@gmail.com',
+    password:             'qgnq trxm tgiv vsqq',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
 end
